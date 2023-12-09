79c1068c3c105087b2dbc0da1ffb2af991131acb
jaumamyfront-ender <nepobedimyjpobedit@gmail.com>
2023-12-09 14:26:38 +0100

    fixissues

--- src/components/Header/sub-header.jsx
+++ src/components/Header/sub-header.jsx
@@ -2,18 +2,24 @@ import React from "react";
 import classes from "./styles/styles.subheader.module.css";
 export default function SubHeader(props) {
   let receivedData = props.info.map((newarray) => (
-    <Elements
-      title={newarray.title}
-      street={newarray.street}
-      city={newarray.city}
-      hours={newarray.hours}
-      days={newarray.days}
-      number={newarray.number}
-      documents={newarray.documents}
-    />
+    <div className={classes.container}>
+      <div className={classes.styles__info}>
+        <h1>{newarray.title}</h1>
+        <p>
+          {newarray.street} {newarray.city}
+        </p>
+      </div>
+      <div className={`${classes.styles__info} ${classes.first}`}>
+        <span>{newarray.hoursEnter}</span>
+        <span>{newarray.hours}</span>
+        <span>{newarray.days}</span>
+      </div>
+      <div className={classes.styles__number}>
+        <button>{newarray.numberOne}</button>
+        <button>{newarray.numberSecond}</button>
+        <button>{newarray.documents}</button>
+      </div>
+    </div>
   ));
   return <div>{receivedData}</div>;
 }
-let Elements = (props) => {
-  return <div></div>;
-};
--- src/redux/state.jsx
+++ src/redux/state.jsx
@@ -1,15 +1,17 @@
 let store = {
   state: {
     HeaderInfoContact: [
-      { id: 0, title: "midori sushi warszawa" },
-      { id: 1, street: "ul.Magiczna" },
-      { id: 2, city: "03-289 Warszawa" },
-      { id: 3, hours: "godziny otwarcia" },
-      { id: 4, hours: "12:00 - 22:00" },
-      { id: 5, days: "poniedzialek - piatek" },
-      { id: 6, number: "(22)401 07 42" },
-      { id: 7, number: "(22)401 07 42" },
-      { id: 8, documents: "Pobierz PDF" },
+      {
+        title: "midori sushi warszawa",
+        street: "ul.Magiczna",
+        city: "03-289 Warszawa",
+        hoursEnter: "godziny otwarcia",
+        hours: "12:00 - 22:00",
+        days: "poniedzialek - piatek",
+        numberOne: "(22)401 07 42",
+        numberSecond: "(22)401 07 42",
+        documents: "Pobierz PDF",
+      },
     ],
   },
 };
